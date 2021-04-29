d6960a11a2a2eacfab6accd93f0e7d699b155d2f
Jannat Rakhi <jannatrakhi31155@gmail.com>
2021-04-29 22:41:26 +0600

    completed

---
+++ FinalTerm-LabTask-7/View/menu.php
@@ -0,0 +1,79 @@
+<?php
+include("headerView.php");
+ ?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Welcome to Bootstrap HomePage</title>
+	<!-- bootstrap cdn -->
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
+	<!-- font awesome cdn -->
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+	<!-- style.css -->
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<!-- jquery cdn -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
+</head>
+<body>
+
+
+
+
+<div class="wrapper">
+
+        <div class="title">
+            <h4><span>fresh food for good health</span>our menu</h4>
+        </div>
+        <div class="menu">
+            <div class="single-menu">
+                <img src="Capture5.PNG" alt="">
+                <div class="menu-content">
+                    <h4>Capachino Coffee <span>$45</span></h4>
+                    <p>Aperiam tempore sit,perferendis numquam repudiandae porro voluptate dicta saepe facilis.</p>
+                </div>
+            </div>
+            <div class="single-menu">
+                <img src="Capture4.PNG" alt="">
+                <div class="menu-content">
+                    <h4>Egged fried salad <span>$64</span></h4>
+                    <p>Aperiam tempore sit,perferendis numquam repudiandae porro voluptate dicta saepe facilis.</p>
+                </div>
+            </div>
+            <div class="single-menu">
+                <img src="Capture8.PNG" alt="">
+                <div class="menu-content">
+                    <h4>chicken fried salad <span>$100</span></h4>
+                    <p>Aperiam tempore sit,perferendis numquam repudiandae porro voluptate dicta saepe facilis.</p>
+                </div>
+            </div>
+            <div class="single-menu">
+                <img src="Capture12.PNG" alt="">
+                <div class="menu-content">
+                    <h4>chicken Rolled with Salad <span>$50</span></h4>
+                    <p>Aperiam tempore sit,perferendis numquam repudiandae porro voluptate dicta saepe facilis.</p>
+                </div>
+            </div>
+            <div class="single-menu">
+                <img src="Capture10.PNG" alt="">
+                <div class="menu-content">
+                    <h4>Burger with Extra Cheeze <span>$60</span></h4>
+                    <p>Aperiam tempore sit,perferendis numquam repudiandae porro voluptate dicta saepe facilis.</p>
+                </div>
+            </div>
+            <div class="single-menu">
+                <img src="Capture13.PNG" alt="">
+                <div class="menu-content">
+                    <h4>Grill Chiken with Souce <span>$45</span></h4>
+                    <p>Aperiam tempore sit,perferendis numquam repudiandae porro voluptate dicta saepe facilis.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
